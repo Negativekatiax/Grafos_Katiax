17bd4a3b20c6918c778842b49c53cf48993e7fc6
Negativekatiax <108428433+Negativekatiax@users.noreply.github.com>
2024-06-20 18:02:53 -0600

    dijkstra visual

--- main.java
+++ main.java
@@ -1,17 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
+
+    private static int[][] grafo;
+    private static int[] distancias;
+    private static boolean[] visitados;
+    private static int[] previos;
+    private static JLabel distanciaLabel;
+    private static JLabel rutaLabel;
+    private static JTextField inicioField;
+    private static JTextField finField;
+    private static JFrame frame;
+    private static boolean highlightPath;
 
     public static void dijkstra(int[][] grafo, int inicio, int fin) {
         int n = grafo.length;
 
-        int[] distancias = new int[n];
+        distancias = new int[n];
         Arrays.fill(distancias, Integer.MAX_VALUE);
         distancias[inicio] = 0;
 
-        boolean[] visitados = new boolean[n];
-        int[] previos = new int[n];
+        visitados = new boolean[n];
+        previos = new int[n];
         Arrays.fill(previos, -1);
 
         for (int i = 0; i < n - 1; i++) {
@@ -37,22 +51,33 @@ public class Main {
                     }
                 }
             }
+
+            // Repaint the graph to visualize the current state
+            SwingUtilities.invokeLater(Main::repaintGraph);
+            try {
+                Thread.sleep(1000); // Pause to see the visualization
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
-        System.out.println("Ruta más corta desde " + (char) ('A' + inicio) + " a " + (char) ('A' + fin) + ":");
-        int v = fin;
         StringBuilder ruta = new StringBuilder();
+        int v = fin;
         while (v != -1) {
             ruta.insert(0, (char) ('A' + v));
             v = previos[v];
             if (v != -1) ruta.insert(0, " -> ");
         }
-        System.out.println(ruta);
-        System.out.println("Distancia mínima: " + distancias[fin]);
+
+        highlightPath = true;
+        SwingUtilities.invokeLater(Main::repaintGraph);
+
+        distanciaLabel.setText("Distancia mínima: " + distancias[fin]);
+        rutaLabel.setText("Ruta más corta: " + ruta);
     }
 
     public static void main(String[] args) {
-        int[][] grafo = {
+        grafo = new int[][]{
                 {0, 1, 0, 2, 0},
                 {1, 0, 3, 0, 0},
                 {0, 3, 0, 4, 0},
@@ -60,15 +85,100 @@ public class Main {
                 {0, 0, 0, 5, 0}
         };
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el vértice de inicio (A, B, C, D, E): ");
-        char inicioChar = scanner.next().charAt(0);
-        int inicio = inicioChar - 'A';
+        frame = new JFrame("Visualización de Dijkstra");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
 
-        System.out.print("Ingrese el vértice final (A, B, C, D, E): ");
-        char finChar = scanner.next().charAt(0);
-        int fin = finChar - 'A';
+        GraphPanel graphPanel = new GraphPanel();
+        frame.setLayout(new BorderLayout());
+        frame.add(graphPanel, BorderLayout.CENTER);
 
-        dijkstra(grafo, inicio, fin);
+        JPanel infoPanel = new JPanel();
+        infoPanel.setLayout(new BoxLayout(infoPanel, BoxLayout.Y_AXIS));
+        distanciaLabel = new JLabel("Distancia mínima: ");
+        rutaLabel = new JLabel("Ruta más corta: ");
+        infoPanel.add(distanciaLabel);
+        infoPanel.add(rutaLabel);
+
+        frame.add(infoPanel, BorderLayout.SOUTH);
+
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout());
+        inputPanel.add(new JLabel("Ingrese el vértice de inicio (A, B, C, D, E): "));
+        inicioField = new JTextField(2);
+        inputPanel.add(inicioField);
+        inputPanel.add(new JLabel("Ingrese el vértice final (A, B, C, D, E): "));
+        finField = new JTextField(2);
+        inputPanel.add(finField);
+        JButton startButton = new JButton("Iniciar");
+        inputPanel.add(startButton);
+
+        frame.add(inputPanel, BorderLayout.NORTH);
+        frame.setVisible(true);
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                char inicioChar = inicioField.getText().charAt(0);
+                int inicio = inicioChar - 'A';
+                char finChar = finField.getText().charAt(0);
+                int fin = finChar - 'A';
+                highlightPath = false;
+                new Thread(() -> dijkstra(grafo, inicio, fin)).start();
+            }
+        });
+    }
+
+    private static void repaintGraph() {
+        frame.repaint();
+    }
+
+    static class GraphPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D g2d = (Graphics2D) g;
+            g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+            int[][] positions = {
+                    {100, 100}, {300, 100}, {500, 100}, {200, 300}, {400, 300}
+            };
+
+            for (int i = 0; i < grafo.length; i++) {
+                for (int j = 0; j < grafo[i].length; j++) {
+                    if (grafo[i][j] != 0) {
+                        g2d.drawLine(positions[i][0], positions[i][1], positions[j][0], positions[j][1]);
+                        g2d.setColor(Color.BLUE);
+                        g2d.drawString(String.valueOf(grafo[i][j]),
+                            (positions[i][0] + positions[j][0]) / 2,
+                            (positions[i][1] + positions[j][1]) / 2);
+                    }
+                }
+            }
+
+            for (int i = 0; i < grafo.length; i++) {
+                if (highlightPath && isInShortestPath(i)) {
+                    g2d.setColor(Color.GREEN);
+                } else if (visitados != null && visitados[i]) {
+                    g2d.setColor(Color.BLUE);
+                } else {
+                    g2d.setColor(Color.RED);
+                }
+                g2d.fillOval(positions[i][0] - 15, positions[i][1] - 15, 30, 30);
+                g2d.setColor(Color.BLACK);
+                g2d.drawString(String.valueOf((char) ('A' + i)), positions[i][0] - 5, positions[i][1] + 5);
+            }
+        }
+
+        private boolean isInShortestPath(int node) {
+            int fin = finField.getText().charAt(0) - 'A';
+            while (fin != -1) {
+                if (fin == node) {
+                    return true;
+                }
+                fin = previos[fin];
+            }
+            return false;
+        }
     }
 }
